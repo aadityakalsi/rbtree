e99a2237ebe9ca89c1a30abdffa40c933cd9efd4
twiddle-bits <twiddlebits.ak@gmail.com>
2017-10-24 22:19:20 -0400

    Add basic bst insert

--- export/rbtree/rbtree.hpp
+++ export/rbtree/rbtree.hpp
@@ -17,15 +17,62 @@ template<class Data, class Comp = std::less<Data>, class Alloc = std::allocator<
 class rbtree
 {
   public:
-    rbtree() : m_root(), m_nil(), m_alloc()
+    rbtree() : m_root(), m_nil(), m_size(0), m_comp(), m_alloc()
     {
         m_nil = create_nil();
     }
 
     ~rbtree()
     {
+        clear();
         destroy_nil(m_nil);
     }
+
+    void clear()
+    {
+        if (m_root) {
+            remove_node(m_root);
+        }
+    }
+
+    bool contains(Data const& x)
+    {
+        Node* n = nullptr;
+        find_parent(x, n);
+        assert(n != nullptr || (!m_comp(n->data(), x) && !m_comp(x, n->data())));
+        return n != nullptr;
+    }
+
+    bool insert(Data const& x)
+    {
+        Node* n = nullptr;
+        auto p = find_parent(x, n);
+        if (n) return false;
+        n = create_node(x);
+        ++m_size;
+        if (!p) {
+            m_root = n;
+            return true;
+        }
+        n->set_parent(p);
+        if (m_comp(x, p->data())) {
+            p->set_left(n);
+        } else {
+            p->set_right(n);
+        }
+        return true;
+    }
+
+    std::size_t size() const
+    {
+        return m_size;
+    }
+
+    bool empty() const
+    {
+        return m_size == 0;
+    }
+
   private:
     enum NodeColor
     {
@@ -40,7 +87,11 @@ class rbtree
         uintptr_t m_parent : sizeof(void*)*8-1;
         Node* m_left;
         Node* m_right;
-
+        // access
+        Data const& data() const
+        {
+            return m_data;
+        }
         NodeColor color() const
         {
             return m_color;
@@ -57,13 +108,14 @@ class rbtree
         {
             return m_left;
         }
+        // modify
         void set_color(NodeColor c)
         {
             m_color = c;
         }
         void set_parent(Node* n)
         {
-            m_parent = n;
+            m_parent = (uintptr_t)n;
         }
         void set_right(Node* n)
         {
@@ -73,6 +125,25 @@ class rbtree
         {
             m_left = n;
         }
+        // relatives
+        Node* grandparent() const
+        {
+            auto p = parent();
+            return p ? p->parent() : nullptr;
+        }
+        Node* sibling() const
+        {
+            auto p = parent();
+            if (!p) return p;
+            return p->left() == this ? p->right() : p->left();
+        }
+        Node* uncle() const
+        {
+            auto p = parent();
+            auto g = p ? p->parent() : nullptr;
+            if (!g) return g;
+            return p == g->left() ? g->right() : g->left();
+        }
     };
 
     using MyAlloc = typename std::allocator_traits<Alloc>::template rebind_alloc<Node>;
@@ -80,6 +151,8 @@ class rbtree
 
     Node* m_root;
     Node* m_nil;
+    std::size_t m_size;
+    Comp m_comp;
     MyAlloc m_alloc;
 
     Node* create_nil()
@@ -98,11 +171,18 @@ class rbtree
         MyAllocTraits::deallocate(m_alloc, node, 1);
     }
 
+    Node* create_node(Data const& x)
+    {
+        return create_node(Data(x));
+    }
+
     Node* create_node(Data&& d)
     {
         auto node = create_nil();
         ::new (static_cast<void*>(&node->m_data)) Data(std::forward<Data>(d));
         node->set_color(RED);
+        node->set_left(m_nil);
+        node->set_right(m_nil);
         return node;
     }
 
@@ -111,6 +191,88 @@ class rbtree
         if (!node) return;
         node->m_data.~Data();
         destroy_nil(node);
+    }
+
+    // actions
+    void rotate_left(Node* n)
+    {
+        auto nnew = n->right();
+        assert(nnew != m_nil);
+
+        // rotate
+        n->set_right(nnew->left());
+        nnew->set_left(n);
+        n->right()->set_parent(n);
+
+        // handle parents
+        auto parent = n->parent();
+        if (parent) {
+            if (parent->right() == n) {
+                parent->set_right(nnew);
+            } else {
+                parent->set_left(nnew);
+            }
+        } else {
+            m_root = nnew;
+        }
+        nnew->set_parent(parent);
+        n->set_parent(nnew);
+    }
+
+    void rotate_right(Node* n)
+    {
+        auto nnew = n->left();
+        assert(nnew != m_nil);
+
+        // rotate
+        n->set_left(nnew->right());
+        nnew->set_right(n);
+        n->left()->set_parent(n);
+
+        // handle parents
+        auto parent = n->parent();
+        if (parent) {
+            if (parent->right() == n) {
+                parent->set_right(nnew);
+            } else {
+                parent->set_left(nnew);
+            }
+        } else {
+            m_root = nnew;
+        }
+        nnew->set_parent(parent);
+        n->set_parent(nnew);
+    }
+
+    Node* find_parent(Data const& x, Node*& next)
+    {
+        Node* p = nullptr;
+        Node* n = m_root;
+        if (!n) return nullptr;
+        while (n != m_nil) {
+            bool const is_less = m_comp(x, n->data());
+            if (!m_comp(n->data(), x) && !is_less) {
+                break;
+            } else if (is_less) {
+                p = n;
+                n = n->left();
+            } else {
+                p = n;
+                n = n->right();
+            }
+        }
+        next = n == m_nil ? nullptr : n;
+        return p;
+    }
+
+    void remove_node(Node* n)
+    {
+        if (n == m_nil) return;
+        auto l = n->left();
+        auto r = n->right();
+        destroy_node(n);
+        remove_node(l);
+        remove_node(r);
     }
 };
 
--- tests/rbt.cpp
+++ tests/rbt.cpp
@@ -27,10 +27,58 @@ using namespace containers;
 void rbt0(void)
 {
     rbtree<int> t;
-    testThat(sizeof(t) == 3*sizeof(void*));
+    testThat(sizeof(t) == 4*sizeof(void*));
+    testThat(t.size() == 0);
+    testThat(t.empty());
+}
+
+void rbt1(void)
+{
+    rbtree<int> t;
+    // queries
+    testThat(t.contains(0) == false);
+    testThat(t.contains(-1) == false);
+    testThat(t.contains(1) == false);
+    // add
+    testThat(t.insert(0) == true);
+    testThat(t.size() == 1);
+    testThat(t.contains(0) == true);
+    testThat(t.contains(-1) == false);
+    testThat(t.contains(1) == false);
+    // add
+    testThat(t.insert(-1) == true);
+    testThat(t.size() == 2);
+    testThat(t.contains(0) == true);
+    testThat(t.contains(-1) == true);
+    testThat(t.contains(1) == false);
+    // add
+    testThat(t.insert(1) == true);
+    testThat(t.size() == 3);
+    testThat(t.contains(0) == true);
+    testThat(t.contains(-1) == true);
+    testThat(t.contains(1) == true);
+    // add-repeat
+    testThat(t.insert(0) == false);
+    testThat(t.size() == 3);
+    testThat(t.contains(0) == true);
+    testThat(t.contains(-1) == true);
+    testThat(t.contains(1) == true);
+    // add-repeat
+    testThat(t.insert(-1) == false);
+    testThat(t.size() == 3);
+    testThat(t.contains(0) == true);
+    testThat(t.contains(-1) == true);
+    testThat(t.contains(1) == true);
+    // add-repeat
+    testThat(t.insert(1) == false);
+    testThat(t.size() == 3);
+    testThat(t.contains(0) == true);
+    testThat(t.contains(-1) == true);
+    testThat(t.contains(1) == true);
 }
 
 setupSuite(rbt)
 {
     addTest(rbt0);
+    addTest(rbt1);
 }
